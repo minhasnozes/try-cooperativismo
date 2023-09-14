93d5b55b2d2636fc54a0aa8bfb0130578c011f9d
Alexandre Soares <alexandregs.dev@gmail.com>
2023-09-14 00:10:53 -0300

    feat: creating NotFoundException message for Assembleia

---
+++ src/main/java/com/example/demo/exceptions/AssembleiaNotFoundException.java
@@ -0,0 +1,6 @@
+package com.example.demo.exceptions;
+
+public class AssembleiaNotFoundException extends RuntimeException {
+
+    public AssembleiaNotFoundException(String message) { super (message); }
+}
